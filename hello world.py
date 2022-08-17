4a6c2c1406b5370164da03187efd9352619e8546
Nikita <ezio13@yandex.ru>
2022-08-17 20:01:56 +0300

    sth to say

--- hello world.py
+++ hello world.py
@@ -1,3 +1,6 @@
+from signal import SIG_DFL
+
+
 print('Hello world!')
 def factorial(n):
     if n == 1:
@@ -11,3 +14,5 @@ print(factorial(8))
 a, b, c = [3] * 3
 
 print(a+b+c)
+
+total = [i for i in range(5)]
